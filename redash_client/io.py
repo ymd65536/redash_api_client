66729a08bb55369324ef1339b75725a1d81e06a5
ymd65536 <ymd65536@gmail.com>
2023-06-08 17:00:50 +0900

    output_sql

--- redash_client/io.py
+++ redash_client/io.py
@@ -1,6 +1,15 @@
 import os
 
+# io module
+
 def output_sql(folder_name, result):
+    """
+
+    Args:
+        folder_name (str): sql save folder name
+        result (json): detail query info 
+    
+    """
     file_name = str(result['id']) + '_' + result['name'] + '.sql'
     dir_path = os.getcwd() + '/' + folder_name
 
@@ -9,3 +18,5 @@ def output_sql(folder_name, result):
 
     with open(dir_path + '/' + file_name, mode='w') as sql_file:
         sql_file.write(result['query'])
+    
+    return {"save_folder": dir_path}
